5a6b2c0d08794d3517ccce2742cbe8353f008f75
Kazi Fahin Abraj Abir <2405007@ugrad.cse.buet.ac.bd>
2025-10-10 09:54:27 +0600

    Refactor string length calculation by adding my_strlen function

--- 101/multi_delim_strtok.c
+++ 101/multi_delim_strtok.c
@@ -2,6 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 
+int my_strlen(const char *str){
+    int length = 0;
+    while(str[length] != '\0'){
+        length++;
+    }
+    return length;
+}
+
 int is_delimiter(char c, const char *delimiters){
     for(int i = 0; delimiters[i] != '\0'; i++){
         if(c == delimiters[i]) return 1;
@@ -16,11 +24,11 @@ char *strtoken_multi(char *str, const char *delimiters){
     
     if(str != NULL){
         if(next != NULL) free(next);
-        next = (char*)malloc(strlen(str)+1);
+        next = (char*)malloc(my_strlen(str)+1);
         strcpy(next, str);
     }
     
-    if(next == NULL || strlen(next) == 0) return NULL;
+    if(next == NULL || my_strlen(next) == 0) return NULL;
     
     // Skip leading delimiters
     while(*next != '\0' && is_delimiter(*next, delimiters)){
@@ -36,8 +44,8 @@ char *strtoken_multi(char *str, const char *delimiters){
     }
     token[i] = '\0';
     
-    if(strlen(token) > 0){
-        ret_token = (char*)malloc(strlen(token)+1);
+    if(my_strlen(token) > 0){
+        ret_token = (char*)malloc(my_strlen(token)+1);
         strcpy(ret_token, token);
         return ret_token;
     }
@@ -48,6 +56,12 @@ char *strtoken_multi(char *str, const char *delimiters){
 int main(){
     char str[200] = "Hello,World;This.is:a test,with;multiple.delimiters";
     char *delimiters = ",.;:";
+    
+    // Test my_strlen function
+    printf("Testing my_strlen function:\n");
+    printf("Length of '%s': %d\n", str, my_strlen(str));
+    printf("Length of '%s': %d\n", delimiters, my_strlen(delimiters));
+    printf("Length of empty string: %d\n\n", my_strlen(""));
     
     printf("Original string: %s\n", str);
     printf("Delimiters: %s\n\n", delimiters);
